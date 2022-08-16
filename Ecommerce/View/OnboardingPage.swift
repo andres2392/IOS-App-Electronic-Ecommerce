fc85e1f60cdbf6c2ecb2fa8878c226eca6feb87c
Andres <andres.gonzalez2392@gmail.com>
2022-08-16 12:44:15 -0400

    init Commit
    init Commit

---
+++ Ecommerce/View/OnboardingPage.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingPage.swift
+//  Ecommerce
+//
+//  Created by PDWS on 8/16/22.
+//
+
+import SwiftUI
+
+struct OnboardingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingPage()
+    }
+}
